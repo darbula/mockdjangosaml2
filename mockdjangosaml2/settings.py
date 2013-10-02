85e9366f8d2a886db3ba470f77eaaec1378e92a4
Damir Arbula <damir.arbula@gmail.com>
2013-10-02 22:28:41 +0200

    default settings change

--- mockdjangosaml2/settings.py
+++ mockdjangosaml2/settings.py
@@ -1,18 +1,18 @@
 from django.conf import settings
 
 
-# Attribute names and values are taken from Croatian AAI@EduHr infrastructure
-# If needed this deafult names and values can be modified in projects 
-# specific MOCK_SAML2_USERS in settings.py  
+# Attribute names and values are taken from Croatian AAI@EduHr infrastructure.
+# If needed, these default names and values can be modified in project 
+# by redefining MOCK_SAML2_USERS in project's settings.py file.
 MOCK_SAML2_USERS = getattr(settings, 'MOCK_SAML2_USERS', {
     'admin@aai-test.hr': {
         'password': 'admin',
         'session_info': {
             'ava': {
+                'hrEduPersonUniqueID': ['admin@aai-test.hr'],
                 'hrEduPersonPrimaryAffiliation': ['djelatnik'],
                 'cn': ['Admin Surname'],
                 'hrEduPersonOIB': ['12345678901'],
-                'hrEduPersonUniqueID': ['asurname@aai-test.hr'],
                 'sn': ['Surname'],
                 'hrEduPersonHomeOrg': ['aai-test.hr'],
                 'mail': ['admin.surname@aai-test.hr'],
@@ -24,10 +24,10 @@ MOCK_SAML2_USERS = getattr(settings, 'MOCK_SAML2_USERS', {
         'password': 'somepwd1',
         'session_info': {
             'ava': {
+                'hrEduPersonUniqueID': ['employee@aai-test.hr'],
                 'hrEduPersonPrimaryAffiliation': ['djelatnik'],
                 'cn': ['Employee Surname'],
                 'hrEduPersonOIB': ['12345678902'],
-                'hrEduPersonUniqueID': ['esurname@aai-test.hr'],
                 'sn': ['Surname'],
                 'hrEduPersonHomeOrg': ['aai-test.hr'],
                 'mail': ['employee.surname@aai-test.hr'],
@@ -39,10 +39,10 @@ MOCK_SAML2_USERS = getattr(settings, 'MOCK_SAML2_USERS', {
         'password': 'somepwd2',
         'session_info': {
             'ava': {
+                'hrEduPersonUniqueID': ['student@aai-test.hr'],
                 'hrEduPersonPrimaryAffiliation': ['student'],
                 'cn': ['Student Surname'],
                 'hrEduPersonOIB': ['12345678903'],
-                'hrEduPersonUniqueID': ['ssurname@aai-test.hr'],
                 'sn': ['Surname'],
                 'hrEduPersonHomeOrg': ['aai-test.hr'],
                 'mail': ['student.surname@aai-test.hr'],
